b1b8df898a71d5f0148628b076626c4070752924
Tano <cristian.cardino@neworientalclub88.com>
2019-04-22 15:05:15 +0800

    Add softdeletes in migrations

--- database/migrations/2019_04_01_106942_create_user_games_table.php
+++ database/migrations/2019_04_01_106942_create_user_games_table.php
@@ -16,12 +16,17 @@ class CreateUserGamesTable extends Migration
         Schema::create('user_games', function (Blueprint $table) {
             $table->bigIncrements('user_game_id');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id', 'user_games_user_id')->references('user_id')->on('users');
             $table->unsignedBigInteger('booking_id');
-            $table->foreign('booking_id', 'user_games_court_booking_id')
-                  ->references('booking_id')->on('user_court_bookings');
             $table->string('status', 50);
             $table->timestamps();
+
+            $table->foreign('user_id', 'user_games_user_id')
+                ->references('user_id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('booking_id', 'user_games_court_booking_id')
+                ->references('booking_id')->on('user_court_bookings');
         });
     }
 
