abba8c21a5b5fdebdcddc6fceac381e28254312a
mastertano <kamiseta.05@gmail.com>
2019-06-08 13:34:15 +0800

    Update env example and tests

--- tests/Feature/CreateCourtTest.php
+++ tests/Feature/CreateCourtTest.php
@@ -20,25 +20,6 @@ class CreateCourtTest extends TestCase
 
     public function testCreateCourt()
     {
-        $abstractUser = \Mockery::mock(User::class);
-
-        $abstractUser->shouldReceive('getId')
-            ->andReturn(1234567890)
-            ->shouldReceive('getName')
-            ->andReturn('Master Tano')
-            ->shouldReceive('getEmail')
-            ->andReturn(str_random(10).'@test.com')
-            ->shouldReceive('getAvatar')
-            ->andReturn('https://en.gravatar.com/userimage');
-
-        $provider = \Mockery::mock('Laravel\Socialite\Contracts\Provider');
-
-        $provider->shouldReceive('user')->andReturn($abstractUser);
-        
-        Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
-
-        $response = $this->get($this->url);
-
-        dd($response->getContent());
+       
     }
 }
